2e6dc77c8b938dbcaa8ed851b381f385631f121b
Sergey Mezentsev <sergeyme@dstack.ai>
2024-02-22 20:32:12 +0300

    Added create_instance support for lambdalabs

--- src/dstack/_internal/core/backends/lambdalabs/compute.py
+++ src/dstack/_internal/core/backends/lambdalabs/compute.py
@@ -18,9 +18,11 @@ from dstack._internal.core.backends.lambdalabs.config import LambdaConfig
 from dstack._internal.core.models.backends.base import BackendType
 from dstack._internal.core.models.instances import (
     InstanceAvailability,
+    InstanceConfiguration,
     InstanceOffer,
     InstanceOfferWithAvailability,
     LaunchedInstanceInfo,
+    SSHKey,
 )
 from dstack._internal.core.models.runs import Job, Requirements, Run
 
@@ -48,6 +50,27 @@ class LambdaCompute(Compute):
         offers_with_availability = self._get_offers_with_availability(offers)
         return offers_with_availability
 
+    def create_instance(
+        self, instance_offer: InstanceOfferWithAvailability, instance_config: InstanceConfiguration
+    ) -> LaunchedInstanceInfo:
+        commands = get_shim_commands(authorized_keys=instance_config.get_public_keys())
+        # shim is asssumed to be run under root
+        launch_command = "sudo sh -c '" + "&& ".join(commands) + "'"
+
+        project_ssh_key = instance_config.ssh_keys[0]
+        user_ssh_key = instance_config.ssh_keys[1]
+
+        return _run_instance(
+            api_client=self.api_client,
+            region=instance_offer.region,
+            instance_type_name=instance_offer.instance.name,
+            user_ssh_public_key=user_ssh_key.public,
+            project_ssh_public_key=project_ssh_key.public,
+            project_ssh_private_key=project_ssh_key.private,
+            instance_name=instance_config.instance_name,
+            launch_command=launch_command,
+        )
+
     def run_job(
         self,
         run: Run,
@@ -56,21 +79,21 @@ class LambdaCompute(Compute):
         project_ssh_public_key: str,
         project_ssh_private_key: str,
     ) -> LaunchedInstanceInfo:
-        commands = get_shim_commands(
-            authorized_keys=[run.run_spec.ssh_key_pub.strip(), project_ssh_public_key.strip()]
+        instance_config = InstanceConfiguration(
+            project_name=run.project_name,
+            instance_name=get_instance_name(run, job),  # TODO: generate name
+            ssh_keys=[
+                SSHKey(
+                    public=project_ssh_public_key.strip(), private=project_ssh_private_key.strip()
+                ),
+                SSHKey(public=run.run_spec.ssh_key_pub.strip()),
+            ],
+            job_docker_config=None,
+            user=run.user,
         )
-        # shim is asssumed to be run under root
-        launch_command = "sudo sh -c '" + "&& ".join(commands) + "'"
-        return _run_instance(
-            api_client=self.api_client,
-            region=instance_offer.region,
-            instance_type_name=instance_offer.instance.name,
-            user_ssh_public_key=run.run_spec.ssh_key_pub,
-            project_ssh_public_key=project_ssh_public_key,
-            project_ssh_private_key=project_ssh_private_key,
-            instance_name=get_instance_name(run, job),
-            launch_command=launch_command,
-        )
+
+        launched_instance_info = self.create_instance(instance_offer, instance_config)
+        return launched_instance_info
 
     def terminate_instance(
         self, instance_id: str, region: str, backend_data: Optional[str] = None
